9c9e7e787e76c51a954bf4e310f6da8faecb2eb8
Manoj Shrees <111334383+Manoj-Shrees@users.noreply.github.com>
2023-09-19 19:46:28 +1000

    v1.3

--- lib/util/strings.dart
+++ lib/util/strings.dart
@@ -2,5 +2,5 @@ class Strings {
   static const String PHONE_PATTERN = r'(^(?:[+0]9)?[0-9]{10,12}$)';
   static const String EMAIL_PATTERN =
       r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$";
-  static const String domain = "royofoods.biz";
+  static const String domain = "manojshrees.dev/foodapp/";
 }
